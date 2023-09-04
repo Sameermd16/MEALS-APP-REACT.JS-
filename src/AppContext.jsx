888f14b8b9dcacc4734da0ae1aac9e3c99b707f3
sameermd16 <sameerthemc@gmail.com>
2023-09-04 17:46:14 +0530

    rendered meals data in meals componenet with axios method, installed react-icons and bootstrap for styling

--- src/AppContext.jsx
+++ src/AppContext.jsx
@@ -1,15 +1,38 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, useContext, useEffect, useState } from 'react'
+import axios from 'axios'
 
 export const AppContext = createContext()
 
+const allMealsURL = "https://www.themealdb.com/api/json/v1/1/search.php?s=A"
+const randomMealsURL = "https://www.themealdb.com/api/json/v1/1/random.php"
 
 export function AppProvider({ children }) {
-    const state = {
-        value: 10,
-        lessCost: 5
-    }
+    
+    const [meals, setMeals] = useState([])
+    console.log(meals)
+
+    useEffect(() => {
+        console.log('appcontext -> useeffect ran')
+        async function fetchData(url) {
+            try {
+                const {data} = await axios.get(url)  
+                // console.log(data.meals)
+                //response from axios is more complex than fetch method
+                // const response = await fetch("https://randomuser.me/api/")
+                // const data = await response.json()
+                // console.log(data)
+                if(data) {
+                    setMeals(data.meals)
+                } 
+            } catch(error) {
+                console.log(error.response)
+            }
+        }
+        fetchData(allMealsURL)
+    }, [])
+
     return (
-        <AppContext.Provider value={state}>
+        <AppContext.Provider value={{ meals }}> 
             { children }
         </AppContext.Provider>
     )
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,11 +1,27 @@
 import { useGlobalContext } from "../AppContext"
-
+import { BsHandThumbsUp } from 'react-icons/bs'
 
 export default function Meals() {
-    
-    const  { value }  = useGlobalContext()
+
+    const { meals }  = useGlobalContext()
+    console.log(meals)
 
     return (
-        <div>this is meals compo: {value} </div>
+        <section className='container-fluid row m-5 gap-4'>
+            {
+                meals.map((object) => {
+                    const { idMeal: id, strMeal: title, strMealThumb: image } = object
+                    return (
+                        <div key={id} className='col shadow'>
+                            <img src={image} alt={title} width='200px' />
+                            <div className='mt-3 d-flex align-items-center justify-content-between'>
+                                <h5> {title} </h5>
+                                <BsHandThumbsUp />
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </section>
     )
 }
